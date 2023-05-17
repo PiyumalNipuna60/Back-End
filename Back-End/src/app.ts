2c394f168815bded404cb2d392ef42c30b31fb62
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-17 11:43:43 +0530

    set DB Connection

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -5,3 +5,12 @@ const app=express();
 
 
 
+db.connect("mongodb+srv://nipuna:1234@test01.kxnbmvm.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("Connected..!");
+    
+    
+}).catch((Error)=>{
+    console.log("Somthing wrong");
+    
+})
