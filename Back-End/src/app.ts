c226fcbc713a306009928fc8b62c4b26202be82a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-17 11:43:56 +0530

    set server port

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -8,7 +8,10 @@ const app=express();
 db.connect("mongodb+srv://nipuna:1234@test01.kxnbmvm.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log("Connected..!");
-    
+    app.listen(5000,()=>{
+        console.log("Server is running");
+        
+    })
     
 }).catch((Error)=>{
     console.log("Somthing wrong");
