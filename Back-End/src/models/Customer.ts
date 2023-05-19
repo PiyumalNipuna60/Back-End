35705b9d47c1dfcd6d5426a9461288a294479678
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-19 22:32:25 +0530

    Add CustomerController

--- Back-End/src/models/Customer.ts
+++ Back-End/src/models/Customer.ts
@@ -1,32 +1,34 @@
+import Module from "module";
 import { Document, Schema, model } from "mongoose";
 
 interface ICustomer extends Document{
-   id:string;
-   name:string;
-   address:string;
-   salary:number;
+  id:string;
+  name:string;
+  address:string;
+  salary:number;
 }
 
-const CustomerSchema = new Schema(
-  {
+const CustomerShema= new Schema(
+{
+  id:{
+    type:String,
+    required:true,
+  },
 
-    id:{
-        type:String,
-        required:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    address:{
-        type:String,
-        required:true,
-    },
-    salary:{
-        type:Number,
-        required:true,
-    }
-  }
-);
+  name:{
+    type:String,
+    required:true,
+  },
 
-export const customer=model<ICustomer>("Customer",CustomerSchema);
+  address:{
+    type:String,
+    required:true,
+  },
+
+  salary:{
+    type:Number,
+    required:true,
+  },
+});
+
+export const customer=model("Customer",CustomerShema);
