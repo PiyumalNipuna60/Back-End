f001895ff1cfaae2fab64987ac70b4df2ba0cd90
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-19 22:46:15 +0530

    Add updateCustomer method

--- Back-End/src/controllers/CustomerController.ts
+++ Back-End/src/controllers/CustomerController.ts
@@ -13,5 +13,12 @@ export default class CustomerController{
         res:Response
         ):Promise<Response> => {
            return res;
-      }
+      };
+
+      updateCustomer:RequestHandler=async (
+        req:Request,
+        res:Response
+        ):Promise<Response> => {
+           return res;
+      };
 }
