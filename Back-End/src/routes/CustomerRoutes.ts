8ecc28b9680005f2a4ccd3df331c0e9d137fb1bb
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-19 23:34:57 +0530

    add constructor

--- Back-End/src/routes/CustomerRoutes.ts
+++ Back-End/src/routes/CustomerRoutes.ts
@@ -5,10 +5,14 @@ export default class CustomerRoutes{
    private router:Router=express.Router();
    private controller:CustomerController=new CustomerController();
 
+   constructor(){
+    this.configRoutes();
+   }
+
    private configRoutes = (): void => {
       this.router.post("/api/v1/customer",this.controller.saveCustomer);
       this.router.get("/api/v1/customer",this.controller.getAllCustomer);
-      this.router.put("/api/v1/customer",this.controller.updateCustomer);
-      this.router.delete("/api/v1/customer",this.controller.deleteCustomer);
+      this.router.put("/api/v1/customer/:id",this.controller.updateCustomer);
+      this.router.delete("/api/v1/customer/:id",this.controller.deleteCustomer);
    };
 }
--- Back-End/src/routes/ItemRoutes.ts
+++ Back-End/src/routes/ItemRoutes.ts
@@ -5,11 +5,15 @@ export default class ItemRoutes{
   private router: Router=Express.Router();
   private controller: ItemController = new ItemController();
 
+  constructor(){
+    this.configRouter();
+  }
+
   private configRouter=():void => {
     this.router.post("/api/v1/item",this.controller.saveItem);
     this.router.get("/api/v1/item",this.controller.getAllItem);
-    this.router.put("/api/v1/item",this.controller.updateItem);
-    this.router.delete("/api/v1/item",this.controller.deleteItem);
+    this.router.put("/api/v1/item/:id",this.controller.updateItem);
+    this.router.delete("/api/v1/item/:id",this.controller.deleteItem);
   }
 
 }
