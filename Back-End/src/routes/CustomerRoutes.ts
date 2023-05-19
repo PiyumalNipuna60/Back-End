4e13ebdc42de314c891fb42972ab7e94f64156b7
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-19 23:21:13 +0530

    method route

--- Back-End/src/routes/CustomerRoutes.ts
+++ Back-End/src/routes/CustomerRoutes.ts
@@ -1,4 +1,15 @@
-import { Express } from "express";
-export default class CustomerRoutes{
+import express, { Router } from "express";
+import CustomerController from "../controllers/CustomerController";
 
+export default class CustomerRoutes{
+   private router:Router=express.Router();
+   private controller:CustomerController=new CustomerController();
+
+   private configRoutes = (): void => {
+      this.router.post("/api/v1/customer",this.controller.saveCustomer);
+      this.router.get("/api/v1/customer",this.controller.getAllCustomer);
+      this.router.put("/api/v1/customer",this.controller.updateCustomer);
+      this.router.delete("/api/v1/customer",this.controller.deleteCustomer);
+      
+   };
 }
