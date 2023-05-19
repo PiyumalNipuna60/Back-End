41d17f96f09e626a83c1378b03b2a08771ad75f2
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-19 23:25:59 +0530

    Add Content

--- Back-End/src/routes/ItemRoutes.ts
+++ Back-End/src/routes/ItemRoutes.ts
@@ -1,3 +1,10 @@
 import  Express, { Router }  from "express";
 import ItemController from "../controllers/ItemControler";
 
+export default class ItemRoutes{
+  private router: Router=Express.Router();
+  private controller: ItemController = new ItemController();
+
+  
+
+}
